eaa3d55615c04a1767e9b064b57dafc3c990f5f1
Tony Tipton <40807075+tttipton@asu.edu>
2019-02-23 20:41:46 -0700

    Initial Version of Calculator

---
+++ CSE 360 Assignment 2/src/cse360assign2/Calculator.java
@@ -0,0 +1,103 @@
+
+package cse360assign2;
+/**
+ * <h1>Calculator</h1>
+ * @author Tony Tipton
+ * @classID 563
+ * @version Assignment 2.0
+ * @since 2019-02-23
+ * 
+ * The Calculator program implements an application that creates a integer variable called total
+ * that is to be manipulated through arithmetic operators add, subtract, multiply, and divide.
+ * The program passes a value that will alter the value stored in total from one of the operators
+ * The program also display the current value stored in total and display the history of operators
+ * that have been executed since the start of the program at any time.
+ * <p>
+ * 
+ */
+
+/**
+ * 
+ * Class Calculator declares and implements a private variable integer value that will be used in
+ * declared methods add(), subtract(), multiply(), and divide(). the add() method will add the
+ * the parameter to the current total, the subtract() method will subtract the the parameter 
+ * from the current total,the multiply() method will multiply the parameter with the current 
+ * total,the divide() method will divide the parameter with the current total
+ *
+ */
+public class Calculator {
+
+	/**
+	 * The private integer to be used in executing the operation methods
+	 */
+	private int total;
+	
+	/**
+	 * Creates a new Calculator object with the given name
+	 * @param There is no parameters
+	 * @return There is no return value
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method prints out the current value stored in variable total and returns
+	 *  a zero when completed
+	 * @param There are no parameters
+	 * @return int This returns an integer 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method is used to add an integer to the current total
+	 * @param value This is the only parameter of the add method which is an integer
+	 * 				that will be added to the current total value
+	 * @return void This returns nothing
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method is used to subtract an integer from the current total
+	 * @param value This is the only parameter of the subtract method which is an integer
+	 * 				that will be added to the current total value
+	 * @return void This returns nothing
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method is used to multiply an integer with the current total
+	 * @param value This is the only parameter of the multiply method which is an integer
+	 * 				that will be added to the current total value
+	 * @return void This returns nothing
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method is used to divide an integer with the current total
+	 * @param value This is the only parameter of the divide method which is an integer
+	 * 				that will be added to the current total value
+	 * @return void This returns nothing
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method is used to display the history of executed operators from the start of
+	 * the program
+	 * @param There is no parameters
+	 * @return String This returns a string containing the history of operations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
